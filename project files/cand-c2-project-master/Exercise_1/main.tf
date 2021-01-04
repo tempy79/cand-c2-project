c7463e0546ee3711a87d618fc3edde249ffdae95
tempy79 <francescoierna@gmail.com>
2021-01-04 16:18:27 +0100

    Update main.tf

--- project files/cand-c2-project-master/Exercise_1/main.tf
+++ project files/cand-c2-project-master/Exercise_1/main.tf
@@ -1,8 +1,6 @@
 # TODO: Designate a cloud provider, region, and credentials
 
 provider "aws" {
-  access_key = "AKIAJPF7S4WDE6Z2SBGA"
-  secret_key = "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"
   region = "us-east-1"
 }
 
